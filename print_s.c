9dbcc4b52a1731385f5710125d5babc30cb6bcef
moha-mame <>
2022-10-16 10:10:45 +0100

    specifiers print commit

---
+++ print_s.c
@@ -0,0 +1,136 @@
+#include "main.h"
+
+/**
+* print_hex - function that prints an unsigned int in hexadecimal
+* @n: unsigned to be printed
+* @c: case of printing (0 = lower, 1 = upper)
+* Descriptions: prints unsigned in hexadecimal with _putchar
+* Return: size the output
+*/
+int print_hex(unsigned int n, unsigned int c)
+{
+	unsigned int len, powten, j, digit, num;
+	int count = 0;
+	char diff;
+
+	if (n != 0)
+	{
+		num = n;
+		len = 0;
+		if (c)
+			diff = 'A' - ':';
+		else
+			diff = 'a' - ':';
+		while (num != 0)
+		{
+			num /= 16;
+			len++;
+		}
+		powten = 1;
+		for (j = 1; j <= len - 1; j++)
+			powten *= 16;
+		for (j = 1; j <= len; j++)
+		{
+			digit = n / powten;
+			if (digit < 10)
+				_putchar(digit + '0');
+			else
+				_putchar(digit + '0' + diff);
+			count++;
+			n -= digit * powten;
+			powten /= 16;
+		}
+	}
+	else
+	{
+		_putchar('0');
+		return (1);
+	}
+	return (count);
+}
+/**
+* print_x - takes an unsigned int an prints it in lowercase hex
+* @x: unsigned int to print
+* Descriptions: prints in lowercase hex with _putchar
+* Return: size of the output
+*/
+int print_x(va_list x)
+{
+	return (print_hex(va_arg(x, unsigned int), 0));
+}
+/**
+* print_X - takes an unsigned int an prints it in uppercase hex
+* @X: unsigned int to print
+* Descriptions: prints in uppercase hex with _putchar
+* Return: size of the output
+*/
+int print_X(va_list X)
+{
+	return (print_hex(va_arg(X, unsigned int), 1));
+}
+
+
+/**
+* print_unsig - function that prints unsigned number
+* @u: unsigned number
+* Descriptions: prints unsigned number with putchar
+* Return: size the output
+*/
+
+int print_unsig(va_list u)
+{
+	unsigned int a[10];
+	unsigned int i, m, n, sum;
+	int count;
+
+	n = va_arg(u, unsigned int);
+	m = 1000000000; /* (10 ^ 9) */
+	a[0] = n / m;
+	for (i = 1; i < 10; i++)
+	{
+		m /= 10;
+		a[i] = (n / m) % 10;
+	}
+	for (i = 0, sum = 0, count = 0; i < 10; i++)
+	{
+		sum += a[i];
+		if (sum || i == 9)
+		{
+			_putchar('0' + a[i]);
+			count++;
+		}
+	}
+	return (count);
+}
+
+/**
+* print_octal - unsigned int argument is converted to unsigned octal
+* @o: unsigned to be converted
+* Descriptions: prints unsigned octal
+* Return: size the output
+*/
+int print_octal(va_list o)
+{
+	unsigned int a[11];
+	unsigned int i, m, n, sum;
+	int count;
+
+	n = va_arg(o, unsigned int);
+	m = 1073741825; /* (8 ^ 10) */
+	a[0] = n / m;
+	for (i = 1; i < 11; i++)
+	{
+		m /= 8;
+		a[i] = (n / m) % 8;
+	}
+	for (i = 0, sum = 0, count = 0; i < 11; i++)
+	{
+		sum += a[i];
+		if (sum || i == 10)
+		{
+			_putchar('0' + a[i]);
+			count++;
+		}
+	}
+	return (count);
+}
