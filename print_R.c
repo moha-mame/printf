2d45149f02c34f2af55cfbdb20903ce68a1eb730
Henok.B <learning4myfuture@gmail.com>
2022-10-18 11:51:59 +0300

    Unkown Files

--- print_R.c
+++ print_R.c
@@ -1,31 +1,24 @@
+/*Task 13*/
 #include "main.h"
-
 /**
-  * print_rot13 - encodes a string into rot13.
-  * @R: string to convert
-  * Return: size the output text
-  */
-int print_rot13(va_list R)
+*print_rev - prints astring in reverse
+*@r: string to print
+*Return: number of chars printed
+*/
+int print_rev(va_list r)
 {
-	int j, i, count = 0;
-	char *r;
-	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz";
-	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLM nopqrstuvwxyzabcdefghijklm";
+	char *st;
+	int i, j = 0;
 
-	r = va_arg(R, char *);
-	if (r == NULL)
-		r = "(null)";
-	for (j = 0; r[j] != '\0'; j++)
+	st = va_arg(r, char *);
+	if (st == NULL)
+		st = ")llun(";
+	for (i = 0; st[i] != '\0'; i++)
+		;
+	for (i -= 1 ; i >= 0; i--)
 	{
-		for (i = 0; input[i] != '\0'; i++)
-		{
-			if (r[j] == input[i])
-			{
-				_putchar(output[i]);
-				count++;
-				break;
-			}
-		}
+		_putchar(st[i]);
+		j++;
 	}
-	return (count);
+	return (j);
 }
